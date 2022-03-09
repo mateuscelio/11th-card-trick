6ff52b57414a580067d505ced73723fc7c65f9b2
mateuscelio <mateuscelio10@gmail.com>
2022-03-09 16:15:19 -0300

    Adds trick logic

--- src/view/TrickTable.js
+++ src/view/TrickTable.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import { generateDeck, shuffleDeck } from "../app";
+import {
+  COLUMNS_SEQUENCE,
+  iterateTrick,
+  columnToCardOffset,
+} from "../app/trick/trick";
 import Card from "./Card";
 import "./TrickTable.css";
 
@@ -7,19 +12,56 @@ export default function TrickTable() {
   const initialDeck = generateDeck();
   const [state, setState] = useState({
     trickDeck: shuffleDeck(initialDeck).slice(0, 21),
+    currentTrickStep: 0,
+    cardChosen: null,
   });
 
-  const renderCards = (offset) => {
-    return state.trickDeck
-      .slice(0 + offset, 7 + offset)
-      .map((card) => <Card card={card} />);
+  const handleColumnClick = (selectedColumn) => {
+    const { orderedDeck, cardChosen, step } = iterateTrick({
+      trickDeck: state.trickDeck.slice(),
+      selectedColumn,
+      currentStep: state.currentTrickStep,
+    });
+    console.log(orderedDeck);
+    setState({
+      trickDeck: orderedDeck,
+      currentTrickStep: step + 1,
+      cardChosen: cardChosen,
+    });
   };
 
-  return (
-    <div class="trick-table">
-      <div class="trick-table__cards">{renderCards(0)}</div>
-      <div class="trick-table__cards">{renderCards(7)}</div>
-      <div class="trick-table__cards">{renderCards(14)}</div>
-    </div>
-  );
+  const renderCards = (column) => {
+    const offset = columnToCardOffset(column);
+    return (
+      <div
+        className="trick-table__cards"
+        onClick={() => handleColumnClick(column)}
+      >
+        {state.trickDeck.slice(offset, 7 + offset).map((card) => (
+          <Card card={card} key={`${card.num}${card.suit}`} />
+        ))}
+      </div>
+    );
+  };
+
+  const renderTrickTable = () => {
+    if (state.cardChosen) {
+      return (
+        <div className="trick-result">
+          <p>The card chosen was</p>
+          <Card card={state.cardChosen} />
+        </div>
+      );
+    } else {
+      return (
+        <div className="trick-table">
+          <>{renderCards(COLUMNS_SEQUENCE.FIRST)}</>
+          <>{renderCards(COLUMNS_SEQUENCE.SECOND)}</>
+          <>{renderCards(COLUMNS_SEQUENCE.THIRD)}</>
+        </div>
+      );
+    }
+  };
+
+  return renderTrickTable();
 }
